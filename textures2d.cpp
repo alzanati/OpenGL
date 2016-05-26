b1549fad8948297f97f3ce5eba7e80fd8fcb0f8f
alzanati <hosny1993.bio@gmail.com>
2016-05-26 19:16:48 +0200

    apply 2d texture to one side of cube

--- textures2d.cpp
+++ textures2d.cpp
@@ -7,7 +7,11 @@
 #define WIDTH 680
 #define HEIGHT 480
 
-float theta = 0.f;
+float angleCube = 0.f;
+float xRotation = 0.f;
+float yRotation = 0.f;
+float zRotation = 0.f;
+bool toggle = false;
 
 GLuint raw_texture_load( const char *filename, int width, int height )
 {
@@ -71,12 +75,13 @@ void initGL()
 void reshape(int w, int h)
 {
   float aspect = (float) w / (float) h;
-  glViewport(0.f, 0.f, float(WIDTH), float(HEIGHT));
+  glViewport(0.f, 0.f, WIDTH, HEIGHT);
 
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
 
-  gluPerspective(45.f, aspect, 0.1f, 100.f);
+  // glOrtho(-3.f, 3.f, -3.f, 3.f, -10.f, 10.f);
+  gluPerspective(45.f, aspect, 1.0f, 100.f);
 }
 
 void Timer(int value)
@@ -85,49 +90,127 @@ void Timer(int value)
   glutTimerFunc(15, Timer, 0);
 }
 
+void keyboard(int key, int x, int y)
+{
+  switch (key)
+  {
+    case GLUT_KEY_LEFT:
+      xRotation -= 0.1;
+      break;
+    case GLUT_KEY_RIGHT:
+      xRotation += 0.1;
+      break;
+    case GLUT_KEY_UP:
+      yRotation += 0.1;
+      break;
+    case GLUT_KEY_DOWN:
+      yRotation -= 0.1;
+      break;
+  }
+
+}
+
 void display()
 {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
 
-  GLuint texture = raw_texture_load( "foot.raw", 256, 256 );
+  // camera transformations
+  gluLookAt(2.5f, 2.5f, 2.5f,
+            xRotation, yRotation, 0.f,
+            0.f ,1.f, 0.f);
 
-  // setup texture mapping
+
+  GLuint texture = raw_texture_load( "texture.raw", 256, 256 );
   glEnable( GL_TEXTURE_2D );
   glBindTexture( GL_TEXTURE_2D, texture );
 
+  // draw axies
+  glBegin(GL_LINES);
+    // x-axis
+    glColor3f(1.f, 0.f, 0.f);
+    glVertex3f(-50.f, 0.f, 0.f);
+    glVertex3f(50.f, 0.f, 0.f);
+    // y-axis
+    glColor3f(0.f, 1.f, 0.f);
+    glVertex3f(0.f, -50.f, 0.f);
+    glVertex3f(0.f, 50.f, 0.f);
+    // z-axis
+    glColor3f(0.f, 0.f, 1.f);
+    glVertex3f(0.f, 0.f, 50.f);
+    glVertex3f(0.f, 0.f, -50.f);
+  glEnd();
+
   glPushMatrix();
-  glRotatef( theta, 0.0f, 0.0f, 1.0f );
+  glRotatef( angleCube, 0.f, 0.f, 1.f );
 
-  glBegin( GL_QUADS );
-    glTexCoord2d(0.0,0.0);
-    glVertex2d(-1.0,-1.0);
+  glBegin(GL_QUADS);
+    glColor3f(0.0f, 1.0f, 0.0f);     // Green
+    glVertex3f( 1.0f, 1.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f,  1.0f);
+    glVertex3f( 1.0f, 1.0f,  1.0f);
 
-    glTexCoord2d(1.0,0.0);
-    glVertex2d(+1.0,-1.0);
+    // Bottom face (y = -1.0f)
+    glColor3f(1.0f, 0.5f, 0.0f);     // Orange
+    glVertex3f( 1.0f, -1.0f,  1.0f);
+    glVertex3f(-1.0f, -1.0f,  1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f( 1.0f, -1.0f, -1.0f);
 
-    glTexCoord2d(1.0,1.0);
-    glVertex2d(+1.0,+1.0);
+    // Front face  (z = 1.0f)
+    glColor3f(1.0f, 0.0f, 0.0f);     // Red
+    glVertex3f( 1.0f,  1.0f, 1.0f);
+    glVertex3f(-1.0f,  1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glVertex3f( 1.0f, -1.0f, 1.0f);
 
-    glTexCoord2d(0.0,1.0);
-    glVertex2d(-1.0,+1.0);
+    // Back face (z = -1.0f)
+    glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
+    glVertex3f( 1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f,  1.0f, -1.0f);
+    glVertex3f( 1.0f,  1.0f, -1.0f);
+
+    // Left face (x = -1.0f)
+    glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+    glVertex3f(-1.0f,  1.0f,  1.0f);
+    glVertex3f(-1.0f,  1.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f,  1.0f);
+
+    // Right face (x = 1.0f)
+    glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
+    glTexCoord3f(1.f, 1.f, 1.f);
+    glVertex3f(1.0f,  1.0f, -1.0f);
+
+    glTexCoord3f(1.f, 1.f, 1.f);
+    glVertex3f(1.0f,  1.0f,  1.0f);
+
+    glTexCoord3f(1.f, -1.f, 1.f);
+    glVertex3f(1.0f, -1.0f,  1.0f);
+
+    glTexCoord3f(1.f, -1.f, 1.f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
   glEnd();
 
   glPopMatrix();
 
-  theta += 0.2;
+    angleCube -= 0.2;
   glutSwapBuffers();
 }
 
 int main(int argc, char** argv)
 {
   glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+  glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE );
   glutInitWindowPosition(50, 50);
   glutInitWindowSize(WIDTH, HEIGHT);
   glutCreateWindow(argv[1]);
   glutDisplayFunc(display);
+  glutReshapeFunc(reshape);
+  glutSpecialFunc(keyboard);
   glutTimerFunc(0, Timer, 0);
   initGL();
   glutMainLoop();
