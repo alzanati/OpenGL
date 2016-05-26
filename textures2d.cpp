8b460e828a3686ad6e988c00addb89ef22e6e85b
alzanati <hosny1993.bio@gmail.com>
2016-05-26 13:51:25 +0200

    basic example texture

--- textures2d.cpp
+++ textures2d.cpp
@@ -7,6 +7,57 @@
 #define WIDTH 680
 #define HEIGHT 480
 
+float theta = 0.f;
+
+GLuint raw_texture_load( const char *filename, int width, int height )
+{
+  GLuint texture;
+  unsigned char *data;
+  FILE *file;
+
+  // open texture data
+  file = fopen(filename, "rb");
+  if (file == NULL)
+    return EXIT_FAILURE;
+
+  // allocate buffer
+  data = (unsigned char*) malloc(width * height * 4);
+
+  // read texture data
+  fread(data, width * height * 4, 1, file);
+  fclose(file);
+
+  // allocate a texture name
+  glGenTextures(1, &texture);
+
+  // select our current texture
+  glBindTexture(GL_TEXTURE_2D, texture);
+
+  // select modulate to mix texture with color for shading
+  glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_DECAL);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_DECAL);
+
+  // when texture area is small, bilinear filter the closest mipmap
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+
+  // when texture area is large, bilinear filter the first mipmap
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  // texture should tile
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+  // build our texture mipmaps
+  gluBuild2DMipmaps( GL_TEXTURE_2D, 4, width, height, GL_RGBA, GL_UNSIGNED_BYTE, data );
+
+  // free buffer
+  free(data);
+
+  return texture;
+}
+
 void initGL()
 {
   glClearColor(0.f, 0.f, 0.f, 0.f);
@@ -28,12 +79,44 @@ void reshape(int w, int h)
   gluPerspective(45.f, aspect, 0.1f, 100.f);
 }
 
+void Timer(int value)
+{
+  glutPostRedisplay();
+  glutTimerFunc(15, Timer, 0);
+}
+
 void display()
 {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
 
+  GLuint texture = raw_texture_load( "foot.raw", 256, 256 );
+
+  // setup texture mapping
+  glEnable( GL_TEXTURE_2D );
+  glBindTexture( GL_TEXTURE_2D, texture );
+
+  glPushMatrix();
+  glRotatef( theta, 0.0f, 0.0f, 1.0f );
+
+  glBegin( GL_QUADS );
+    glTexCoord2d(0.0,0.0);
+    glVertex2d(-1.0,-1.0);
+
+    glTexCoord2d(1.0,0.0);
+    glVertex2d(+1.0,-1.0);
+
+    glTexCoord2d(1.0,1.0);
+    glVertex2d(+1.0,+1.0);
+
+    glTexCoord2d(0.0,1.0);
+    glVertex2d(-1.0,+1.0);
+  glEnd();
+
+  glPopMatrix();
+
+  theta += 0.2;
   glutSwapBuffers();
 }
 
@@ -45,6 +128,7 @@ int main(int argc, char** argv)
   glutInitWindowSize(WIDTH, HEIGHT);
   glutCreateWindow(argv[1]);
   glutDisplayFunc(display);
+  glutTimerFunc(0, Timer, 0);
   initGL();
   glutMainLoop();
   return EXIT_SUCCESS;
