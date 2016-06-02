909e9aba73fe813a014040c379ed396c69c96a78
alzanati <hosny1993.bio@gmail.com>
2016-06-02 14:19:09 +0200

    basic texture example

--- startV/headers/transform.vert
+++ startV/headers/transform.vert
@@ -1,11 +1,17 @@
 #version 300 es
 
-in vec2 texcoord;
+layout(location = 0) in vec2 position;
+layout(location = 1) in vec3 color;
+layout(location = 2) in vec2 texcoord;
 
 out vec3 Color;
 out vec2 Texcoord;
+out vec2 v_position;
 
 void main()
 {
+    Color = color;
     Texcoord = texcoord;
+    v_position = Texcoord;
+    gl_Position = vec4(position, 0.f, 1.f);
 }
--- startV/src/main.cpp
+++ startV/src/main.cpp
@@ -8,6 +8,32 @@
 #include <SOIL/SOIL.h>
 #include "shaders.hpp"
 
+// Shader sources
+const GLchar* vertexSource =
+    "#version 150 core\n"
+    "in vec2 position;"
+    "in vec3 color;"
+    "in vec2 texcoord;"
+    "out vec3 Color;"
+    "out vec2 Texcoord;"
+    "void main()"
+    "{"
+    "    Color = color;"
+    "    Texcoord = texcoord;"
+    "    gl_Position = vec4(position, 0.0, 1.0);"
+    "}";
+const GLchar* fragmentSource =
+    "#version 150 core\n"
+    "in vec3 Color;"
+    "in vec2 Texcoord;"
+    "out vec4 outColor;"
+    "uniform sampler2D texKitten;"
+    "uniform sampler2D texPuppy;"
+    "void main()"
+    "{"
+    "    outColor = mix(texture(texKitten, Texcoord), texture(texPuppy, Texcoord), 0.5);"
+    "}";
+
 float timed = 0.f;
 
 int main(int argc, char** argv)
@@ -46,62 +72,16 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
     }
 
-    GLuint program = LoadShaders("/home/mohamed/workspace/OpenGl/startV/simple.vert",
-                                 "/home/mohamed/workspace/OpenGl/startV/simple.frag");
+    // Create Vertex Array Object
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
 
-    // transform feedback
-    const char* varying[] = {"v_position"};
-    glTransformFeedbackVaryings(program, 1, varying, GL_INTERLEAVED_ATTRIBS);
-    glLinkProgram(program);
-    glUseProgram(program);
+    // Create a Vertex Buffer Object and copy the vertex data to it
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
 
-    GLuint vertex_buffer;
-    GLuint color_buffer;
-    GLuint sVertex_buffer;
-    GLuint sColor_buffer;
-    GLuint feedBack_buffer;
-    GLuint texture_buffer;
-
-    glGenBuffers(1, &vertex_buffer);
-    glGenBuffers(1, &color_buffer);
-    glGenBuffers(1, &sVertex_buffer);
-    glGenBuffers(1, &sColor_buffer);
-    glGenBuffers(1, &feedBack_buffer);
-    glGenTextures(1, &texture_buffer);
-
-    GLuint vertex_array[2];
-    glGenVertexArrays(2, vertex_array);
-
-    const GLfloat vertices[] =
-    {
-        -0.5f, -0.5f, 0.3f,
-        0.5f, -0.5f, 0.1f,
-        0.5f, 0.5f, 0.2f,
-    };
-    const GLfloat colors[]=
-    {
-        0.0f, 0.0f, 1.0f,
-        0.0f, 1.0f, 0.0f,
-        1.0f, 0.0f, 0.0f,
-    };
-
-    const GLfloat sVertices[] =
-    {
-        -0.6f, 0.2f, 0.f,
-        -0.2f, 0.2f, 0.f,
-        -0.2f, 0.6f, 0.f,
-        -0.6f, 0.6f, 0.f,
-    };
-    const GLfloat sColors[]=
-    {
-        0.0f, 0.7f, 0.5f,
-        0.1f, 0.1f, 0.1f,
-        1.0f, 0.0f, 0.0f,
-        1.0f, 0.4f, 1.0f
-    };
-
-    /* textures */
-    float vertices[] = {
+    GLfloat vertices[] = {
     //  Position      Color             Texcoords
         -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Top-left
          0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // Top-right
@@ -109,128 +89,95 @@ int main(int argc, char** argv)
         -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f  // Bottom-left
     };
 
-    glBindTexture(GL_TEXTURE_2D, texture_buffer);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-    float color[] = { 1.0f, 0.0f, 0.0f, 1.0f };
-    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, color);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glGenerateMipmap(GL_TEXTURE_2D);
-
-    int width, height;
-    char* path = "/home/mohamed/workspace/OpenGl/startV/seg3.png";
-    unsigned char* image = SOIL_load_image(path, &width, &height,
-                                           0, SOIL_LOAD_RGB);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
-                 GL_UNSIGNED_BYTE, image);
-    SOIL_free_image_data(image);
-
-    /* bind to VAO */
-    /* so you can bind all vertexs to one vao then use it in any location */
-    glBindVertexArray(vertex_array[0]);
-
-    // bind buffers
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+    // Create an element array
+    GLuint ebo;
+    glGenBuffers(1, &ebo);
 
-    GLint position_attrib = glGetAttribLocation(program, "position");
-    glEnableVertexAttribArray(position_attrib);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glVertexAttribPointer(position_attrib, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    GLuint elements[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
 
-    GLint color_attrib = glGetAttribLocation(program, "color_in");
-    glEnableVertexAttribArray(color_attrib);
-    glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
-    glVertexAttribPointer(color_attrib, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    // Create and compile the vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    glCompileShader(vertexShader);
 
-    //tbo /* glEnable(GL_RASTERIZER_DISCARD); disable rendering */
-    glBindBuffer(GL_ARRAY_BUFFER, feedBack_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), NULL, GL_STATIC_READ);
-    glBindVertexArray(0);
+    // Create and compile the fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glCompileShader(fragmentShader);
 
-    /* square data */
-    glBindVertexArray(vertex_array[1]);
+    // Link the vertex and fragment shader into a shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
 
-    glBindBuffer(GL_ARRAY_BUFFER, sVertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(sVertices), sVertices, GL_STATIC_DRAW);
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
 
-    glBindBuffer(GL_ARRAY_BUFFER, sColor_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(sColors), sColors, GL_STATIC_DRAW);
+    // Specify the layout of the vertex data
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glEnableVertexAttribArray(posAttrib);
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), 0);
 
-    glEnableVertexAttribArray(position_attrib);
-    glBindBuffer(GL_ARRAY_BUFFER, sVertex_buffer);
-    glVertexAttribPointer(position_attrib, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+    glEnableVertexAttribArray(colAttrib);
+    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
 
-    glEnableVertexAttribArray(color_attrib);
-    glBindBuffer(GL_ARRAY_BUFFER, sColor_buffer);
-    glVertexAttribPointer(color_attrib, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-    glBindVertexArray(0);
+    GLint texAttrib = glGetAttribLocation(shaderProgram, "texcoord");
+    glEnableVertexAttribArray(texAttrib);
+    glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)(5 * sizeof(GLfloat)));
 
-    // transformations
-    glm::mat4 rotate;
-    rotate = glm::rotate(rotate, glm::degrees(20.f), glm::vec3(0.0f, 0.0f, 1.0f));
+    // Load textures
+    GLuint textures;
+    glGenTextures(1, &textures);
 
-    glm::mat4 trans;
-    trans = glm::translate(trans, glm::vec3(0.5f, -0.3f, 0.0f));
+    int width, height;
+    unsigned char* image;
 
-    glm::mat4 scale;
-    scale = glm::scale(scale, glm::vec3(0.5f, 1.f, 0.5));
-    glm::mat4 model = scale * trans * rotate;
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, textures);
+        image = SOIL_load_image("/home/prof/workspace/OpenGL/startV/seg3.png",
+                                &width, &height, 0, SOIL_LOAD_RGB);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+        SOIL_free_image_data(image);
+    glUniform1i(glGetUniformLocation(shaderProgram, "texKitten"), 0);
 
-    // uniform model matrix
-    GLint uniTrans = glGetUniformLocation(program, "trans");
-    glUniformMatrix4fv(uniTrans, 1, GL_FALSE, glm::value_ptr(rotate));
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-    // 1 for no rotation, 0 for rotation
-    GLint state = glGetUniformLocation(program, "state");
-    glUniform1i(state, 1);
-
-    // Perform feedback transform
-    glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, position_attrib, feedBack_buffer);
-
-    /* unbind to this vertex array */
-    glEnable(GL_DEPTH_TEST);
-
-    /* start rendering */
+    // start rendering
     while(!glfwWindowShouldClose(window))
     {
         // Clear the screen to black
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-        glBeginTransformFeedback(GL_TRIANGLES);
-            glBindVertexArray(vertex_array[0]);
-            glDrawArrays(GL_TRIANGLES, 0, 4);
-            glBindVertexArray(0);
-        glEndTransformFeedback();
-
-        glBindVertexArray(vertex_array[1]);
-        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-        glBindVertexArray(0);
+        // Draw a rectangle from the 2 triangles using 6 indices
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
         glFlush();
     }
 
-    // dispaly data
-    float* data = (float*) malloc(sizeof(vertices));
-    glGetBufferSubData(GL_TRANSFORM_FEEDBACK_BUFFER, 0, sizeof(vertices), data);
-    for(int i = 0; i < sizeof(vertices) / sizeof(float); i++)
-        std::cout << data[i] << std::endl;
+    glDeleteProgram(shaderProgram);
+    glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShader);
 
-    //clean up the memories
-    glDisableVertexAttribArray(position_attrib);
-    glDisableVertexAttribArray(color_attrib);
-    glDeleteBuffers(1, &vertex_buffer);
-    glDeleteBuffers(1, &color_buffer);
-    glDeleteVertexArrays(1, vertex_array);
-    glDeleteProgram(program);
+    glDeleteBuffers(1, &ebo);
+    glDeleteBuffers(1, &vbo);
+
+    glDeleteVertexArrays(1, &vao);
 
     // Close OpenGL window and terminate GLFW
     glfwDestroyWindow(window);
