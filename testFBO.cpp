554770eebb6a9c445b49b005b23fcdec4fcb4244
alzanati <hosny1993.bio@gmail.com>
2016-07-02 12:51:04 +0200

    finaly fbo and pbo works correctly to readback data

--- testFBO.cpp
+++ testFBO.cpp
@@ -1,173 +1,162 @@
-/**
-@AUTHORS: Mohamed Hosny Ahmed
-**/
-
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <stdio.h>
 #include <GL/glew.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <iostream>
+#endif
 
+#define WIDTH 512
+#define HIEGHT 512
 
-#define WIDTH 680
-#define HEIGHT 480
+#define m_WIDTH 256
+#define m_HIEGHT 256
 
-int m_width = 300;
-int m_height = 300;
+#define BUFFER_OFFSET(i) ((char *)NULL + (i))
 
-float angleCube = 0.f;
-float xRotation = 0.f;
-float yRotation = 0.f;
-float zRotation = 0.f;
-bool toggle = false;
+GLuint fbo, color_tex, depth_rb, pbo[2];
 
-GLuint fb, color, depth;
+void changeSize(int w, int h) {
 
-void glInit()
-{
-  glClearColor(0.f, 0.f, 0.f, 0.f);
-  glEnable(GL_DEPTH_TEST);
-  glClearDepth(1.f);
-}
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window of zero width).
+	if (h == 0)
+		h = 1;
 
-void reshape(int w, int h)
-{
-  float aspect = (float) w / (float) h;
-  glViewport(0, 0, WIDTH, HEIGHT);
+	float ratio =  w * 1.0 / h;
 
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
+	// Use the Projection Matrix
+	glMatrixMode(GL_PROJECTION);
 
-  // glOrtho(-3.f, 3.f, -3.f, 3.f, -10.f, 10.f);
-  gluPerspective(45.f, aspect, 1.f, 100.f);
-}
+	// Reset Matrix
+	glLoadIdentity();
 
-void Timer(int value)
-{
-  glutPostRedisplay();
-  glutTimerFunc(15, Timer, 0);
-}
+	// Set the viewport to be the entire window
+	glViewport(0, 0, w, h);
 
-void keyboard(int key, int x, int y)
-{
-  switch (key)
-  {
-    case GLUT_KEY_LEFT:
-      xRotation -= 0.1;
-      break;
-    case GLUT_KEY_RIGHT:
-      xRotation += 0.1;
-      break;
-    case GLUT_KEY_UP:
-      yRotation += 0.1;
-      break;
-    case GLUT_KEY_DOWN:
-      yRotation -= 0.1;
-      break;
-  }
-}
+	// Set the correct perspective.
+	gluPerspective(45,ratio,1,100);
 
-void CHECK_FRAMEBUFFER_STATUS()
-{  
-  GLenum status;
-  status = glCheckFramebufferStatus(GL_FRAMEBUFFER_EXT); 
-  switch(status) 
-  {
-    case GL_FRAMEBUFFER_COMPLETE:
-      break;
-
-    case GL_FRAMEBUFFER_UNSUPPORTED:
-    /* choose different formats */
-      break;
-
-    default:
-      /* programming error; will fail on all hardware */
-      fputs("Framebuffer Error\n", stderr);
-      exit(-1);
-  }                                                     
+	// Get Back to the Modelview
+	glMatrixMode(GL_MODELVIEW);
 }
 
 void initFBO()
 {
-  glGenTextures(1, &color);
-  glBindTexture(GL_TEXTURE_2D, color);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_width, m_height, 0, GL_BGRA, GL_UNSIGNED_BYTE, NULL);
+	glGenTextures(1, &color_tex);
+  	glBindTexture(GL_TEXTURE_2D, color_tex);
+  	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_WIDTH, m_HIEGHT, 0, GL_BGRA, GL_UNSIGNED_BYTE, NULL);
+  	glBindTexture(GL_TEXTURE_2D, 0);
 
-  glGenFramebuffers(1, &fb);
-  glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fb);
-  glFramebufferTexture2D(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, color, 0);
+  	glGenFramebuffersEXT(1, &fbo);
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo);
+    glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, GL_TEXTURE_2D, color_tex, 0);
+   
+    glGenRenderbuffersEXT(1, &depth_rb);
+    glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, depth_rb);
+    glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, GL_DEPTH_COMPONENT24, 256, 256);
 
-  CHECK_FRAMEBUFFER_STATUS();
+  	glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, GL_TEXTURE_2D, color_tex, 0);
+	glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT, GL_RENDERBUFFER_EXT, depth_rb);
+
+  	GLenum status;
+    status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT);
+    if( status )
+    	std::cout << "good" << std::endl;
+    else
+    	std::cout << "bad" << std::endl;
 }
 
-void display()
+void initPBO()
 {
-  // attach our framebuffer
-  glBindFramebuffer(GL_FRAMEBUFFER_EXT, fb);
-
-  glViewport(0, 0, m_width, m_height);
-  glClearColor(0 , 0, 0, 0);
-  glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-
-  glDisable(GL_TEXTURE_2D);
-  glDisable(GL_BLEND);
-  glEnable(GL_DEPTH_TEST);
-
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-
-  glPushMatrix();
-    glColor3f(0.4f, 1.0f, 0.0f);
-    glTranslatef(-0.5, -0.5, -5);
-    glutWireSphere(0.8, 20, 20);
-  glPopMatrix();
-
-  // attach default frame buffer
-  glBindFramebuffer(GL_FRAMEBUFFER_EXT, 0);
-  glClearColor(0.1f, 0.1f, 0.1f, 0.f);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  glViewport(0, 0, WIDTH, HEIGHT);
-
-  glEnable(GL_TEXTURE_2D);
-  glBindTexture(GL_TEXTURE_2D, color);
-
-  glMatrixMode( GL_MODELVIEW );
-  glLoadIdentity();
-  glTranslatef(-0.1, -0.1, -2.0);
-
-  glBegin(GL_QUADS);
-    glTexCoord2f(0.0, 0.0); glVertex3f(0.0, 0.0, 0.0);
-    glTexCoord2f(1.0, 0.0); glVertex3f(1.0, 0.0, 0.0);
-    glTexCoord2f(1.0, 1.0); glVertex3f(1.0, 1.0, 0.0);
-    glTexCoord2f(0.0, 1.0); glVertex3f(0.0, 1.0, 0.0);
-  glEnd();
-  
-  glutSwapBuffers();
+	glGenBuffers(2, pbo);
 }
 
-int main(int argc, char** argv)
+void renderScene(void) 
 {
-  int mode = GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE;
-  glutInit(&argc, argv);
-  glutInitDisplayMode(mode);
-  glutInitWindowSize(WIDTH, HEIGHT);
-  glutCreateWindow(argv[1]);
-  glutDisplayFunc(display);
-  glutReshapeFunc(reshape);
-  glutSpecialFunc(keyboard);
+	int index = (index + 1) % 2;
+	int nextIndex = (index + 1) % 2;
 
-  glewInit();
-  printf("ARB FBO: %i\n",glewIsSupported("GL_ARB_framebuffer_object"));
-  printf("EXT FBO: %i\n",glewIsSupported("GL_EXT_framebuffer_object"));
+	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.0, 0.0, 0.0, 1);
+	glViewport(0, 0, m_WIDTH, m_HIEGHT);
 
-  glInit();
-  initFBO();
+	glBindBuffer(GL_PIXEL_PACK_BUFFER_ARB, pbo[0]);
+    glBufferData(GL_PIXEL_PACK_BUFFER_ARB, m_WIDTH * m_HIEGHT * 4, NULL, GL_STREAM_READ);
 
-  glutTimerFunc(0, Timer, 0);
-  glutMainLoop();
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1, 1, -1, 1, -1, 1);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glBegin(GL_TRIANGLES);
+		glColor3f(1.f, 0.f, 0.f);
+		glVertex3f(0, 0.5, 0.0);
+		glColor3f(0.f, 1.f, 0.f);
+		glVertex3f(0.5, 0, 0.0);
+		glColor3f(0.f, 0.f, 1.f);
+		glVertex3f(-0.5, 0, 0.0);
+	glEnd();
+	
+	/* so to read pixels correctly we need to draw view port with specific width and height */
+	/* and spacify the formate so color will be mapped to bytes */
+	/* for example if width = 256, height = 256, type is unsigned char, and formate RGBA */
+	/* will read at every time a pixel stored in 4 channels, so 4 unsigned char will mapped represent it */
+	/* so complete from read pixels 256 * 256 * 4 * sizeof(unsigned char) */
+
+	// unsigned char* ptr = (unsigned char*) malloc(sizeof(unsigned char) * m_WIDTH * m_HIEGHT * 4);
+	// glReadPixels(0, 0, m_WIDTH, m_WIDTH, GL_BGRA, GL_UNSIGNED_BYTE, ptr);
+	// for(int i = 0; i < 256 * 256 * 4; i++)
+	// 	std::cout << (int)ptr[i] << std::endl;
+
+	glBindBuffer(GL_PIXEL_PACK_BUFFER_ARB, pbo[0]);
+	glReadPixels(0, 0, m_WIDTH, m_HIEGHT, GL_BGRA, GL_UNSIGNED_BYTE, BUFFER_OFFSET(0));
+
+	glBindBuffer(GL_PIXEL_PACK_BUFFER_ARB, pbo[0]);
+	unsigned char* ptr = (unsigned char*)glMapBufferARB(GL_PIXEL_PACK_BUFFER_ARB, GL_READ_ONLY);
+	if(ptr)
+	{
+		for(int i = 0; i < 256 * 256 * 4; i++)
+			std::cout << (int)ptr[i] << std::endl;
+	}
+
+}
+
+
+int main(int argc, char **argv) 
+{
+
+	// init GLUT and create window
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(512, 512);
+	glutCreateWindow("Lighthouse3D - GLUT Tutorial");
+	glutDisplayFunc(renderScene);
+	glutReshapeFunc(changeSize);
+	
+	glewInit();
+  	printf("ARB FBO: %i\n",glewIsSupported("GL_ARB_framebuffer_object"));
+  	printf("EXT FBO: %i\n",glewIsSupported("GL_EXT_framebuffer_object"));
+
+	initFBO();
+	initPBO();
+
+	// register callbacks
+	
+	//glutReshapeFunc(changeSize);
+	renderScene();
+	// enter GLUT event processing loop
+	//glutMainLoop();
+
+	return 1;
 }
